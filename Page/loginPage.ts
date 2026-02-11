5c029e2701432ea2a3d8aedd174db18d35b2f688
Dexter Dwayne Tennison <83023757+Enduren@users.noreply.github.com>
2026-02-10 18:22:41 -0600

    first commit

---
+++ Page/loginPage.ts
@@ -0,0 +1,27 @@
+import { Page, Locator, expect } from '@playwright/test';
+
+export class LoginPage {
+  readonly page: Page;
+  readonly usernameInput: Locator;
+  readonly passwordInput: Locator;
+  readonly loginButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.usernameInput = page.getByPlaceholder('Username');
+    this.passwordInput = page.getByPlaceholder('Password');
+    this.loginButton = page.getByRole('button', { name: 'Login' });
+  }
+
+  async goto() {
+    await this.page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  }
+
+  async login(user: string, pass: string) {
+    await this.usernameInput.fill(user);
+    await this.passwordInput.fill(pass);
+    await this.loginButton.click();
+    // Wait for the dashboard to load to ensure login worked
+    await expect(this.page).toHaveURL(/dashboard/);
+  }
+}
---
+++ tests/employee-lifecycle.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../Page/loginPage';
+
+test.describe('Employee Lifecycle E2E', () => {
+
+  test('should login, add a new employee, and verify record', async ({ page }) => {
+    const loginPage= new LoginPage(page)
+    
+    // const firstName = 'John';
+    // const lastName = `Doe_${Date.now()}`;
+    
+    // 1. Login
+    await loginPage.goto();
+    await loginPage.login('Admin', 'admin123');
+
+    // 2. Navigate to PIM
+
+    // 3. Add Employee (First Name, Last Name, ID)
+
+    // 4. Save and Search for the new employee
+
+    // 5. Assert the employee appears in the list
+  });
+
+});
